9c1bd6a22548b5aaeb161621988692aa5f8bdb1f
Louis J Rabeno <lrabeno@gmail.com>
2024-04-03 13:05:45 -0400

    addUser function

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,51 @@
+import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { addUser } from '../features/users';
 
 export const Home = () => {
-  const username = useSelector((state) => state.user.value.username);
-  return <h1> THIS IS THE HOME PAGE {username}</h1>;
+  const [name, setName] = useState('');
+  const [username, setUsername] = useState('');
+
+  const userList = useSelector((state) => state.users.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <div className="addUser">
+        <input
+          type="text"
+          placeholder="Name..."
+          onChange={(event) => setName(event.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Username..."
+          onChange={(event) => setUsername(event.target.value)}
+        />
+        <button
+          onClick={() => {
+            dispatch(
+              addUser({
+                id: userList[userList.length - 1].id + 1,
+                name: name,
+                username: username,
+              })
+            );
+          }}
+        >
+          Add User
+        </button>
+      </div>
+      <div className="displayUsers">
+        {userList.map((user) => {
+          return (
+            <div key={user.id}>
+              <h1>{user.name}</h1>
+              <h2>{user.username}</h2>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
--- src/store.js
+++ src/store.js
@@ -1,24 +1,8 @@
 import { configureStore, createSlice } from '@reduxjs/toolkit';
-
-const initialState = { value: { username: '' } };
-
-const userSlice = createSlice({
-  name: 'user',
-  initialState,
-  reducers: {
-    login: (state, action) => {
-      state.value = action.payload;
-    },
-    logout: (state) => {
-      state.value = initialState.value;
-    },
-  },
-});
-
-export const { login, logout } = userSlice.actions;
+import usersReducer from './features/users';
 
 export const store = configureStore({
   reducer: {
-    user: userSlice.reducer,
+    users: usersReducer,
   },
 });
